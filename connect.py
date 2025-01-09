dea767afcdb80ee69e5e4309aafff65c7f1982fc
Ku Ho Ming <koman1437@yahoo.com>
2025-01-09 19:57:47 +0800

    update key

--- connect.py
+++ connect.py
@@ -7,11 +7,10 @@ import os
 
 def inference():
 
-    ACCESS_TOKEN = "sk-10efaa4d38dc4cee919b30b229e843a0"
     client = OpenAI(
     # If the environment variable is not configured, replace the following line with: api_key="sk-xxx",
-        api_key="sk-10efaa4d38dc4cee919b30b229e843a0", 
-        base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1",
+        api_key="sk-71a04cbb628c4c96bdf7d8da50d9ea18", 
+        base_url="https://api.deepseek.com/v1",
     )
 
     def query(system_message, human_message):
